98e4d0fca6622baadfc9fdd2324b9e18b2fa689f
Deepak <deepak@telehealer.com>
2019-01-18 18:20:55 +0530

    Signature add, remove , edit added with api integration. #163361336 - Issues mentioned in this id related to signature capture is fixed.

---
+++ app/src/main/java/com/thealer/telehealer/apilayer/models/signature/SignatureApiViewModel.java
@@ -0,0 +1,56 @@
+package com.thealer.telehealer.apilayer.models.signature;
+
+import android.app.Application;
+import android.graphics.Bitmap;
+import android.support.annotation.NonNull;
+
+import com.thealer.telehealer.apilayer.baseapimodel.BaseApiResponseModel;
+import com.thealer.telehealer.apilayer.baseapimodel.BaseApiViewModel;
+import com.thealer.telehealer.common.CameraUtil;
+import com.thealer.telehealer.views.base.BaseViewInterface;
+
+/**
+ * Created by Aswin on 18,January,2019
+ */
+public class SignatureApiViewModel extends BaseApiViewModel {
+    public SignatureApiViewModel(@NonNull Application application) {
+        super(application);
+    }
+
+    public void uploadSignature(boolean isShowProgress, Bitmap signature) {
+        fetchToken(new BaseViewInterface() {
+            @Override
+            public void onStatus(boolean status) {
+                if (status) {
+                    String path = CameraUtil.with(getApplication()).getBitmapFilePath(signature);
+                    getAuthApiService().uploadSignature(getMultipartFile("signature", path))
+                            .compose(applySchedulers())
+                            .subscribe(new RAObserver<BaseApiResponseModel>(getProgress(isShowProgress)) {
+                                @Override
+                                public void onSuccess(BaseApiResponseModel baseApiResponseModel) {
+                                    baseApiResponseModelMutableLiveData.setValue(baseApiResponseModel);
+                                }
+                            });
+                }
+            }
+        });
+    }
+
+    public void deleteSignature(boolean isShowProgress) {
+        fetchToken(new BaseViewInterface() {
+            @Override
+            public void onStatus(boolean status) {
+                if (status) {
+                    getAuthApiService().deleteSignature()
+                            .compose(applySchedulers())
+                            .subscribe(new RAObserver<BaseApiResponseModel>(getProgress(isShowProgress)) {
+                                @Override
+                                public void onSuccess(BaseApiResponseModel baseApiResponseModel) {
+                                    baseApiResponseModelMutableLiveData.setValue(baseApiResponseModel);
+                                }
+                            });
+                }
+            }
+        });
+    }
+}
