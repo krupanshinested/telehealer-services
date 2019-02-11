3aed2432d5f907a0dcabad9f36c1e72ddd34513f
Deepak <deepak@telehealer.com>
2019-02-11 19:05:18 +0530

    Notification with badge counted added. Badge count update through broadcast added.

--- app/src/main/java/com/thealer/telehealer/common/Constants.java
+++ app/src/main/java/com/thealer/telehealer/common/Constants.java
@@ -136,6 +136,7 @@ public class Constants {
     public static final int forRegistration = 1;
     public static final int forProfileUpdate = 2;
     public static final String DATE_PICKER_CANCELLED = "datePickerCancelled";
+    public static final String NOTIFICATION_COUNT_RECEIVER = "notification_count_receiver";
     public static String male = "male";
     public static String female = "female";
 
