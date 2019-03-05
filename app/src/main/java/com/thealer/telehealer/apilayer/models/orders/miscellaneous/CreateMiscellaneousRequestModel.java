0c72cb93eb6952459818d914065a44f395846414
Deepak <deepak@telehealer.com>
2019-03-05 17:40:28 +0530

    Miscellaneous order creation completed. Miscellaneous order detail view completed. Miscellaneous Empty state icon updated.

---
+++ app/src/main/java/com/thealer/telehealer/apilayer/models/orders/miscellaneous/CreateMiscellaneousRequestModel.java
@@ -0,0 +1,35 @@
+package com.thealer.telehealer.apilayer.models.orders.miscellaneous;
+
+/**
+ * Created by Aswin on 05,March,2019
+ */
+public class CreateMiscellaneousRequestModel {
+
+    private String user_guid;
+    private String name;
+    private MiscellaneousDetailBean detail;
+
+    public String getUser_guid() {
+        return user_guid;
+    }
+
+    public void setUser_guid(String user_guid) {
+        this.user_guid = user_guid;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public MiscellaneousDetailBean getDetail() {
+        return detail;
+    }
+
+    public void setDetail(MiscellaneousDetailBean detail) {
+        this.detail = detail;
+    }
+}
