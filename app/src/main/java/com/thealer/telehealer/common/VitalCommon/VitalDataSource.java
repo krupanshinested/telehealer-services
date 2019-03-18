eb89ae3769f5b154d473cda3c91a6825aa97c984
Deepak <deepak@telehealer.com>
2019-03-18 20:16:15 +0530

    Fixes for the Ease display and Graph. Added Device Image and Connecting Gif.

---
+++ app/src/main/java/com/thealer/telehealer/common/VitalCommon/VitalDataSource.java
@@ -0,0 +1,38 @@
+package com.thealer.telehealer.common.VitalCommon;
+
+import com.thealer.telehealer.common.BaseAdapterObjectModel;
+
+public class VitalDataSource extends BaseAdapterObjectModel {
+    private String deviceType;
+    private String measurementType;
+
+    public String getDeviceType() {
+        return deviceType;
+    }
+
+    public void setDeviceType(String deviceType) {
+        this.deviceType = deviceType;
+    }
+
+    public String getMeasurementType() {
+        return measurementType;
+    }
+
+    public void setMeasurementType(String measurementType) {
+        this.measurementType = measurementType;
+    }
+
+    public VitalDataSource(String deviceType, String measurementType) {
+        this.deviceType = deviceType;
+        this.measurementType = measurementType;
+    }
+
+    @Override
+    public String getAdapterTitle() {
+        return  measurementType;
+    }
+
+    @Override
+    public Object getComparableObject() { return measurementType;  }
+
+}
