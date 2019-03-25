a668e799935a2d6179d7b3c7528240d9a252ce3c
Deepak <deepak@telehealer.com>
2019-03-25 15:39:36 +0530

    Diet pdf generation completed.

--- app/src/main/java/com/thealer/telehealer/views/home/monitoring/diet/DietConstant.java
+++ app/src/main/java/com/thealer/telehealer/views/home/monitoring/diet/DietConstant.java
@@ -1,5 +1,8 @@
 package com.thealer.telehealer.views.home.monitoring.diet;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+
 /**
  * Created by Aswin on 21,February,2019
  */
@@ -12,4 +15,11 @@ public class DietConstant {
     public static final String TYPE_LUNCH = "Lunch";
     public static final String TYPE_DINNER = "Dinner";
     public static final String TYPE_SNACKS = "Snacks";
+
+    public static final String PRINT_1_WEEK = "Last 1 weeks";
+    public static final String PRINT_2_WEEK = "Last 2 weeks";
+    public static final String PRINT_1_MONTH = "Last 1 Month";
+    public static final String DIET_PRINT_ALL = "All Diet History";
+    public static final ArrayList<String> dietPrintOptions = new ArrayList<>(Arrays.asList(PRINT_1_WEEK, PRINT_2_WEEK, PRINT_1_MONTH, DIET_PRINT_ALL));
+
 }
