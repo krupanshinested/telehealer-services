910220101b3776593cf155d736d7990d852651ba
Deepak <deepak@telehealer.com>
2019-07-15 16:41:38 +0530

    Access log completed.

---
+++ app/src/main/java/com/thealer/telehealer/apilayer/models/accessLog/AccessLogApiViewModel.java
@@ -0,0 +1,37 @@
+package com.thealer.telehealer.apilayer.models.accessLog;
+
+import android.app.Application;
+
+import androidx.annotation.NonNull;
+
+import com.thealer.telehealer.apilayer.baseapimodel.BaseApiResponseModel;
+import com.thealer.telehealer.apilayer.baseapimodel.BaseApiViewModel;
+import com.thealer.telehealer.common.Constants;
+import com.thealer.telehealer.views.base.BaseViewInterface;
+
+/**
+ * Created by Aswin on 15,July,2019
+ */
+public class AccessLogApiViewModel extends BaseApiViewModel {
+    public AccessLogApiViewModel(@NonNull Application application) {
+        super(application);
+    }
+
+    public void getAccessLog(int page, boolean isShowProgress) {
+        fetchToken(new BaseViewInterface() {
+            @Override
+            public void onStatus(boolean status) {
+                if (status) {
+                    getAuthApiService().getAccessLogs(page, Constants.PAGINATION_SIZE, "GET")
+                            .compose(applySchedulers())
+                            .subscribe(new RAObserver<BaseApiResponseModel>(getProgress(isShowProgress)) {
+                                @Override
+                                public void onSuccess(BaseApiResponseModel baseApiResponseModel) {
+                                    baseApiResponseModelMutableLiveData.setValue(baseApiResponseModel);
+                                }
+                            });
+                }
+            }
+        });
+    }
+}
