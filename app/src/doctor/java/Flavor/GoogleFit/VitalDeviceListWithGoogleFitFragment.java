18a1c6be5c27a87dfd329680c122d3243ee323ea
Deepak <deepak@telehealer.com>
2019-08-13 11:19:55 +0530

    Google fit enhancement Enable Vital creation for MA

---
+++ app/src/doctor/java/Flavor/GoogleFit/VitalDeviceListWithGoogleFitFragment.java
@@ -0,0 +1,7 @@
+package Flavor.GoogleFit;
+
+import com.thealer.telehealer.views.base.BaseFragment;
+import com.thealer.telehealer.views.home.vitals.iHealth.pairing.VitalDeviceListFragment;
+
+public class VitalDeviceListWithGoogleFitFragment extends VitalDeviceListFragment {
+}
