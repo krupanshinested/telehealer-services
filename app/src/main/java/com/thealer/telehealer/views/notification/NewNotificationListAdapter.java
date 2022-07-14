2525ab38d489d984d54942b7d7cd5a3a8d60d5d4
Pratik Gajera <pratikgajera@knovator.in>
2022-07-14 18:50:36 +0530

    1. View subscription in notification session

--- app/src/main/java/com/thealer/telehealer/views/notification/NewNotificationListAdapter.java
+++ app/src/main/java/com/thealer/telehealer/views/notification/NewNotificationListAdapter.java
@@ -67,6 +67,7 @@ public class NewNotificationListAdapter extends RecyclerView.Adapter<NewNotifica
     public static final String REQUEST_TYPE_CONNECTION = "connection";
     public static final String REQUEST_TYPE_APPOINTMENT = "appointment";
     private final String REQUEST_TYPE_MISSED_CALL = "missed_call";
+    private final String SUBSCRIPTION = "subscription";
     private final String REQUEST_TYPE_CALLS = "calls";
     private final String REQUEST_TYPE_ABNORMAL_VITAL = "vitals";
     private final String MESSAGES = "messages";
@@ -240,6 +241,13 @@ public class NewNotificationListAdapter extends RecyclerView.Adapter<NewNotifica
                     case OrderConstant.ORDER_TYPE_FORM:
                         title = activity.getString(R.string.form).toUpperCase();
                         description = resultModel.getMessage();
+                        viewHolder.descriptionTv.setVisibility(View.VISIBLE);
+                        viewHolder.bottomView.setVisibility(View.VISIBLE);
+                        break;
+                    case SUBSCRIPTION:
+                        title = activity.getString(R.string.subscription).toUpperCase();
+                        description = resultModel.getMessage();
+                        viewHolder.userDetailCl.setVisibility(View.GONE);
                         viewHolder.descriptionTv.setVisibility(View.VISIBLE);
                         viewHolder.bottomView.setVisibility(View.VISIBLE);
                         break;
